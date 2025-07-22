69fd6993671c0df66e3671df4bd75889ab0b25e6
Getonto3 <getontomoses18@gmail.com>
2025-07-22 23:32:45 +0300

    feat: the view for a trip

--- trips/views.py
+++ trips/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+"""Views for the accounts app."""
 
-# Create your views here.
+from rest_framework import generics
+from .serializers import TripSerializer
+from .models import Trip
+
+
+class TripPlannerView(generics.ListCreateAPIView):
+    """view for creating a new trip"""
+    
+    queryset = Trip.objects.all()
+    serializer_class = TripSerializer
