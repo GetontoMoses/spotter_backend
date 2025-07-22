6c390f1fab8fec70dd77ca2bf98fc3d26392ce7d
Getonto3 <getontomoses18@gmail.com>
2025-07-22 23:11:27 +0300

    feat: Trip model

--- trips/models.py
+++ trips/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Trip(models.Model):
+    current_location = models.CharField(max_length=100)
+    pickup_location = models.CharField(max_length=100)
+    dropoff_location = models.CharField(max_length=100)
+    cycle_hours_used = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.current_location} → {self.dropoff_location}"
