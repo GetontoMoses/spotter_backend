e5b302317ae8b0129942a59e12f0711802989d8c
Getonto3 <getontomoses18@gmail.com>
2025-07-22 23:37:24 +0300

    feat: url for the trip

--- trips/urls.py
+++ trips/urls.py
@@ -1,5 +1,7 @@
 """URL Configuration for the trips app."""
 
 from django.urls import path
+from .views import TripPlannerView
 
-urlpatterns = [path("")]
+
+urlpatterns = [path("create/", TripPlannerView.as_view(), name="new_trip")]
