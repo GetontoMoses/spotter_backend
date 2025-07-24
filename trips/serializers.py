86342711cc16c89e0514d2ed6352785f191f2d97
Getonto3 <getontomoses18@gmail.com>
2025-07-24 21:32:46 +0300

    feat: Dailylog serializer

--- trips/serializers.py
+++ trips/serializers.py
@@ -1,10 +1,16 @@
 """Serializers for the trips app."""
 
 from rest_framework import serializers
-from .models import Trip
+from .models import Trip, DailyLog
 
 
 class TripSerializer(serializers.ModelSerializer):
     class Meta:
         model = Trip
         fields = "__all__"
+
+
+class DailyLogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DailyLog
+        fields = ["trip", "day", "hour", "status"]
