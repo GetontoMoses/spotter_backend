7050ab68e58dd2343e2516cb19ca7719d0f8b7ff
Getonto3 <getontomoses18@gmail.com>
2025-07-24 23:40:51 +0300

    feat: urls

--- trips/urls.py
+++ trips/urls.py
@@ -1,7 +1,20 @@
 """URL Configuration for the trips app."""
 
 from django.urls import path
-from .views import TripPlannerView
+from .views import (
+    TripListCreateView,
+    LocationUpdateListCreateView,
+    TripLogsByTripView,
+    DailyLogListView,
+)
 
-
-urlpatterns = [path("create/", TripPlannerView.as_view(), name="new_trip")]
+urlpatterns = [
+    path("create/", TripListCreateView.as_view(), name="trip-list-create"),
+    path(
+        "locations/",
+        LocationUpdateListCreateView.as_view(),
+        name="location-update-list-create",
+    ),
+    path("logs/<int:trip_id>/", TripLogsByTripView.as_view(), name="trip-logs"),
+    path("dailylogs/", DailyLogListView.as_view(), name="daily-logs"),
+]
--- trips/views.py
+++ trips/views.py
@@ -1,52 +1,72 @@
 """Views for the accounts app."""
 
-from rest_framework import generics,status
-from rest_framework.response import response
-from .serializers import TripSerializer
-from .models import Trip
-from .utils import get_route_info, plan_fuel_stops, generate_eld_logs
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Trip, LocationUpdate, DailyLog
+from .serializers import TripSerializer, LocationUpdateSerializer, DailyLogSerializer
+import requests
+from rest_framework import status
+import datetime
+from config.settings.utils import get_env_variable
 
+ORS_API_KEY = get_env_variable("ORS_API_KEY")
 
-class TripPlannerView(generics.ListCreateAPIView):
-    """
-    Handles POST requests to plan a trip and return route info and ELD logs.
-    """
-
+class TripListCreateView(ListCreateAPIView):
     queryset = Trip.objects.all()
     serializer_class = TripSerializer
 
-    def create(self, request, *args, **kwargs):
-        # Serialize and validate the input data
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
+    def perform_create(self, serializer):
         trip = serializer.save()
+        try:
+            route_data = self.get_route(trip.pickup_location, trip.dropoff_location)
 
-        # Get routing data from utils.py
-        route_data = get_route_info(
-            trip.current_location,
-            trip.pickup_location,
-            trip.dropoff_location
-        )
+            trip.total_distance = (
+                route_data.get("features")[0]["properties"]["summary"]["distance"]
+                / 1609.34
+            )
+            trip.estimated_hours = (
+                route_data.get("features")[0]["properties"]["summary"]["duration"]
+                / 3600
+            )
+            trip.save()
 
-        if not route_data:
-            return Response({"error": "Failed to get route. Please check locations."},
-                            status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            print(f"Error fetching route: {e}")
 
-        # Generate fuel stops and logs
-        fuel_stops = plan_fuel_stops(route_data["distance_km"])
-        eld_logs = generate_eld_logs(
-            total_drive_hours=route_data["duration_hr"],
-            cycle_hours_used=trip.cycle_hours_used
-        )
+    def get_route(self, start, end):
+        start_coords = self.geocode(start)
+        end_coords = self.geocode(end)
 
-        # Create combined response
-        return Response({
-            "trip": serializer.data,
-            "route": {
-                "distance_km": route_data["distance_km"],
-                "duration_hr": route_data["duration_hr"],
-                "coordinates": route_data["geometry"],
-                "estimated_fuel_stops": fuel_stops
-            },
-            "eld_logs": eld_logs
-        }, status=status.HTTP_201_CREATED)
+        url = "https://api.openrouteservice.org/v2/directions/driving-car"
+        headers = {"Authorization": ORS_API_KEY}
+        params = {
+            "start": f"{start_coords[0]},{start_coords[1]}",
+            "end": f"{end_coords[0]},{end_coords[1]}",
+        }
+        response = requests.get(url, headers=headers, params=params)
+        return response.json()
+
+    def geocode(self, location_name):
+        url = "https://api.openrouteservice.org/geocode/search"
+        params = {"api_key": ORS_API_KEY, "text": location_name}
+        response = requests.get(url, params=params).json()
+        coords = response["features"][0]["geometry"]["coordinates"]
+        return coords  # [longitude, latitude]
+
+
+class LocationUpdateListCreateView(ListCreateAPIView):
+    queryset = LocationUpdate.objects.all()
+    serializer_class = LocationUpdateSerializer
+
+
+class DailyLogListView(ListCreateAPIView):
+    queryset = DailyLog.objects.all()
+    serializer_class = DailyLogSerializer
+
+
+class TripLogsByTripView(APIView):
+    def get(self, request, trip_id):
+        updates = LocationUpdate.objects.filter(trip_id=trip_id).order_by("timestamp")
+        serializer = LocationUpdateSerializer(updates, many=True)
+        return Response(serializer.data)
